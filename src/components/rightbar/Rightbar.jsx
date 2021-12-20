0fb31109b3192a3a1a255cf02083778efea6261b
Akash <akash.0711a@gmail.com>
2021-12-20 14:26:29 +0530

    added a nft corner

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -4,7 +4,16 @@ import './rightbar.css';
 const Rightbar = () => {
     return (
         <div className = "rightbar">
-            Rightbar
+            <div className = "rightbarWrapper">
+                <div className = "NFTContainer">
+                    <img className = "NFTpic" src = "/assets/nft.jfif" alt = ""/>
+                    <span className = "NFTText">NFT corner</span>
+                </div>
+                <span className = "nodtext">NFT of the day  </span>
+                <img className className = "NFToftheDay" src = "/assets/NOD.jfif "/>
+                <h4 className = "rightbarTitle">Online Friends</h4>
+                
+            </div>
         </div>
     )
 }
