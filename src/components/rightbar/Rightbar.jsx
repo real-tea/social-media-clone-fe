237153a3a67b7033cd48efae132e208cd371c5f5
Akash <akash.0711a@gmail.com>
2021-12-21 14:41:03 +0530

    rightbar changed

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -11,8 +11,18 @@ const Rightbar = () => {
                 </div>
                 <span className = "nodtext">NFT of the day  </span>
                 <img className className = "NFToftheDay" src = "/assets/NOD.jfif "/>
-                <h4 className = "rightbarTitle">Online Friends</h4>
-                
+                <h4 className = "rightbarTitle">Suggestions for you</h4>
+                <ul className = "rightbarFriendList">
+                    <li className = "rightbarFriend">
+                        <div className = "rightImgContainer">
+                            <img className = "rightbarProfile" src = "/assets/person/3.jfif" alt = ""/>
+                            <span className = "rightbarFollow"></span>
+                        </div>
+                        <span className = "rightbarUsername">Thakela Chintu</span>
+                        <i className = "followRightbar">Follow +</i>
+                    </li>
+                    
+                </ul>
             </div>
         </div>
     )
